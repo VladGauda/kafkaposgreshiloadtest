9086903d23ad2b225bdbdc54a703738bfd0465f0
Vladimir <vladimir.malyshkin@gmail.com>
2023-09-21 11:37:13 +0300

    configs

---
+++ src/main/java/ru/zen0n/kafkapostgreshiloadtest/KafkaPostgresHiLoadTestApplication.java
@@ -0,0 +1,13 @@
+package ru.zen0n.kafkapostgreshiloadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPostgresHiLoadTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPostgresHiLoadTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zen0n/kafkapostgreshiloadtest/KafkaPostgresHiLoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zen0n.kafkapostgreshiloadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPostgresHiLoadTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
